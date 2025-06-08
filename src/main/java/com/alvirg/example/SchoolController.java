46578ffba2561bef68e65c12b15471b4e6b14a16
albertegi <albertegi90@gmail.com>
2025-06-08 03:06:04 +0100

    updated SchoolDto

--- src/main/java/com/alvirg/example/SchoolController.java
+++ src/main/java/com/alvirg/example/SchoolController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SchoolController {
@@ -13,16 +14,33 @@ public class SchoolController {
     private final SchoolRepository schoolRepository;
 
     public SchoolController(SchoolRepository schoolRepository) {
+
         this.schoolRepository = schoolRepository;
     }
 
     @PostMapping("/schools")
-    public School create(@RequestBody School school){
-        return schoolRepository.save(school);
+    public SchoolDto create(@RequestBody SchoolDto schoolDto){
+
+        var school = toSchool(schoolDto);
+        schoolRepository.save(school);
+        return schoolDto;
+    }
+
+    private School toSchool(SchoolDto schoolDto){
+        return new School(schoolDto.name());
+    }
+
+
+    private SchoolDto toSchoolDto(School school){
+        return new SchoolDto(school.getName());
     }
 
     @GetMapping("/schools")
-    public List<School> findAllStudent(){
-        return schoolRepository.findAll();
+    public List<SchoolDto> findAllStudent(){
+
+        return schoolRepository.findAll()
+                .stream()
+                .map(this::toSchoolDto)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/alvirg/example/SchoolDto.java
@@ -0,0 +1,6 @@
+package com.alvirg.example;
+
+public record SchoolDto(
+        String name
+) {
+}
