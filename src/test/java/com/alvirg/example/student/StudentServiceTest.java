9728046ae8efc8293729b8ca2ff34aed6a0647dc
albertegi <albertegi90@gmail.com>
2025-06-12 03:04:49 +0100

    mocked the StudentServiceTest class

--- src/test/java/com/alvirg/example/student/StudentServiceTest.java
+++ src/test/java/com/alvirg/example/student/StudentServiceTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentServiceTest {
 
     // Which service we want to test? create an object of that service
     @InjectMocks
     private StudentService studentService;
+
+    // declare the dependencies
 
     @Mock
     StudentRepository studentRepository;
@@ -39,10 +42,24 @@ class StudentServiceTest {
         Student student = new Student(
                 "Jane",
                 "Doe",
-                "markcollins@gmail.com",
+                "janedoe@gmail.com",
                 20);
 
+        Student savedStudent = new Student(
+                "Jane",
+                "Doe",
+                "markcollins@gmail.com",
+                20);
+        savedStudent.setId(1);
+
         // Mock the calls: Mock every call that uses another dependency
+        Mockito.when(studentMapper.toStudent(studentDto)).thenReturn(student);
+        Mockito.when(studentRepository.save(student)).thenReturn(savedStudent);
+        Mockito.when(studentMapper.toStudentResponseDto(savedStudent))
+                .thenReturn(new StudentResponseDto(
+                        "John",
+                        "Doe",
+                        "johndoe@gmail.com"));
 
         // When
         StudentResponseDto studentResponseDto = studentService.saveStudent(studentDto);
